e064cec1d112886318c97d14e8ae579d6a963365
Peparon <peparoncino@gmail.com>
2021-12-19 21:31:30 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'd97388c1dd0ff4f1c4b5e752e7da6d9853a0cf395ae3736518724ea1338e5b696372a7fb9770b4c81b3fb5439d9df5b74802a31a3bd6c22cf680376149c39282'
